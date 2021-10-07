61a2ced32a09ed9c4298a57f8ba46b5cf1a18ef1
Moh_RizkiRamdani <60476092+mohamadrizki725@users.noreply.github.com>
2021-10-07 12:44:44 +0700

    menyelesaikan Blade Templating Engine

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,18 +14,71 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Mohamad Rizki Ramdani",
         "email" => "mohamadrizki@gmail.com",
         "image" => "rizki.jpg"
     ]);
 });
 
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Mohamad Rizki Ramdani",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui eum, hic doloribus vel provident natus obcaecati expedita eligendi quos fugiat dolorum porro voluptas quae animi consectetur aut a quas laborum eos enim tenetur blanditiis? Voluptatibus cupiditate sint possimus! Ipsam quam dolorem ut fugiat similique ducimus, amet ad voluptates adipisci sit sunt odio porro, quo possimus veniam, eligendi aspernatur quae sapiente corporis facere accusantium vel doloribus! Illum nesciunt laudantium perferendis aspernatur, mollitia tenetur provident minima dolorum facere quisquam reprehenderit, possimus qui."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Moh Rizki",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui eum, hic doloribus vel provident natus obcaecati expedita eligendi quos fugiat dolorum porro voluptas quae animi consectetur aut a quas laborum eos enim tenetur blanditiis? Voluptatibus cupiditate sint possimus! Ipsam quam dolorem ut fugiat similique ducimus, amet ad voluptates adipisci sit sunt odio porro, quo possimus veniam, eligendi aspernatur quae sapiente corporis facere accusantium vel doloribus! Illum nesciunt laudantium perferendis aspernatur, mollitia tenetur provident minima dolorum facere quisquam reprehenderit, possimus qui."
+        ],
+        ]; 
+        
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
 });
  
+// halaman single post
+
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Mohamad Rizki Ramdani",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui eum, hic doloribus vel provident natus obcaecati expedita eligendi quos fugiat dolorum porro voluptas quae animi consectetur aut a quas laborum eos enim tenetur blanditiis? Voluptatibus cupiditate sint possimus! Ipsam quam dolorem ut fugiat similique ducimus, amet ad voluptates adipisci sit sunt odio porro, quo possimus veniam, eligendi aspernatur quae sapiente corporis facere accusantium vel doloribus! Illum nesciunt laudantium perferendis aspernatur, mollitia tenetur provident minima dolorum facere quisquam reprehenderit, possimus qui."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Moh Rizki",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui eum, hic doloribus vel provident natus obcaecati expedita eligendi quos fugiat dolorum porro voluptas quae animi consectetur aut a quas laborum eos enim tenetur blanditiis? Voluptatibus cupiditate sint possimus! Ipsam quam dolorem ut fugiat similique ducimus, amet ad voluptates adipisci sit sunt odio porro, quo possimus veniam, eligendi aspernatur quae sapiente corporis facere accusantium vel doloribus! Illum nesciunt laudantium perferendis aspernatur, mollitia tenetur provident minima dolorum facere quisquam reprehenderit, possimus qui."
+        ],
+        ]; 
+
+        foreach($blog_posts as $post) {
+            if($post["slug"] === $slug) {
+                $new_post = $post;
+            }
+        }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
+});
