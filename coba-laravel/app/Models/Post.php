897f2cf4b74da9a3be07b304efe7d3b125b3266a
Moh_RizkiRamdani <60476092+mohamadrizki725@users.noreply.github.com>
2021-10-14 21:13:00 +0700

    Menyelesaikan Post Model

--- coba-laravel/app/Models/Post.php
+++ coba-laravel/app/Models/Post.php
@@ -2,34 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-class Post 
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Mohamad Rizki Ramdani",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui eum, hic doloribus vel provident natus obcaecati expedita eligendi quos fugiat dolorum porro voluptas quae animi consectetur aut a quas laborum eos enim tenetur blanditiis? Voluptatibus cupiditate sint possimus! Ipsam quam dolorem ut fugiat similique ducimus, amet ad voluptates adipisci sit sunt odio porro, quo possimus veniam, eligendi aspernatur quae sapiente corporis facere accusantium vel doloribus! Illum nesciunt laudantium perferendis aspernatur, mollitia tenetur provident minima dolorum facere quisquam reprehenderit, possimus qui."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Moh Rizki",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui eum, hic doloribus vel provident natus obcaecati expedita eligendi quos fugiat dolorum porro voluptas quae animi consectetur aut a quas laborum eos enim tenetur blanditiis? Voluptatibus cupiditate sint possimus! Ipsam quam dolorem ut fugiat similique ducimus, amet ad voluptates adipisci sit sunt odio porro, quo possimus veniam, eligendi aspernatur quae sapiente corporis facere accusantium vel doloribus! Illum nesciunt laudantium perferendis aspernatur, mollitia tenetur provident minima dolorum facere quisquam reprehenderit, possimus qui."
-        ]
-        ];
+    use HasFactory;
 
-        public static function all()
-        {
-            return collect(self::$blog_posts);
-        }
-
-        public static function find($slug)
-        {
-            $posts = static::all();
-            
-            return $posts->firstWhere('slug', $slug);
-        }
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
