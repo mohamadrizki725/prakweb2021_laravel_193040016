5aa2d059350d581cec5684ce0175d6c723ffc028
Moh_RizkiRamdani <60476092+mohamadrizki725@users.noreply.github.com>
2021-10-28 18:19:48 +0700

    Menyelesaikan Searching & Pagination

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -8,7 +8,7 @@
             <h1 class="mb-3">{{ $title }}</h1>
 
             
-             <p>By. <a href="/author/{{$post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+             <p>By. <a href="/blog?author={{$post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/blog?categories={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
             
             <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
 
