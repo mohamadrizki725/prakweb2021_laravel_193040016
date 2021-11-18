d9a01be18a221a164189a66726dd09098c0cefc3
Moh_RizkiRamdani <60476092+mohamadrizki725@users.noreply.github.com>
2021-11-18 22:21:36 +0700

    Menyelesaikan Update & Delete Post

--- coba-laravel/resources/views/dashboard/posts/show.blade.php
+++ coba-laravel/resources/views/dashboard/posts/show.blade.php
@@ -9,10 +9,16 @@
             
            <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all my Post</a>
 
-           <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+           <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
 
-           <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
-            
+         
+
+           <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+            @method('delete')
+            @csrf
+            <button class="btn btn-danger" onclick="return confirm('Are You Sure?')"><span data-feather="x-circle"></span> Delete </button>
+          </form>
+
             <img src="https://source.unsplash.com/1200x400?{{  $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
 
             <article class="my-3 fs-5">
